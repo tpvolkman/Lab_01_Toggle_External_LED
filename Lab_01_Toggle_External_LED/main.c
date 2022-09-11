ed4bf6ad59680bf1f27cf0c78e077a0759b1ae72
Thomas Volkman <51494553+tpvolkman@users.noreply.github.com>
2022-09-10 21:40:52 -0500

    Add files via upload

---
+++ Lab_01_Toggle_External_LED/main.c
@@ -0,0 +1,33 @@
+/*
+ *	Toggle_Uno_LED.c
+ * 
+ *	Author: Professor Volkman
+ *  Description: This application toggles the LED on the UNO Development Board
+ */
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB |= 0x20;				// (1 << DDB5)
+	DDRC |= 0x01;
+
+	while (1)
+	{
+		if (( PINB & 0x20 ) == 0x20 )
+		{
+			PORTB &= ~( 0x20 );
+			PORTC |= 0x01;
+		}
+		else
+		{
+			PORTB |= 0x20;
+			PORTC &= ~( 0x01 );
+		}
+
+		_delay_ms( 500 );
+	}
+}
+
